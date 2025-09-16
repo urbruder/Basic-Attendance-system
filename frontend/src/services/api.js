c1ef442184f1f0fb352ff6292d11d5f5bf01a4b8
urbruder <harshsingh310105@gmail.com>
2025-09-17 00:24:54 +0530

    Done

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://basic-attendance-system-backend.vercel.app',
 });
 
 api.interceptors.request.use((config) => {
